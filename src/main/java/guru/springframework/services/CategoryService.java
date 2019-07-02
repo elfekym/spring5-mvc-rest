d156794a21a5b52c99ec2f8d3cb5b11e9f94eb18
Mohamed <mohamed.e@trufla.com>
2019-07-02 21:15:59 +0200

    added @Service annotation to ServiceImpl, both endpoints are working proberly now

--- src/main/java/guru/springframework/services/CategoryService.java
+++ src/main/java/guru/springframework/services/CategoryService.java
@@ -2,8 +2,10 @@ package guru.springframework.services;
 
 import guru.springframework.Domain.Category;
 import guru.springframework.api.v1.model.CategoryDTO;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+
 
 public interface CategoryService {
 
