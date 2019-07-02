c465fb1c6712d31033c4de2120bccfe036cd95ac
Mohamed <mohamed.e@trufla.com>
2019-07-02 21:13:18 +0200

    Added Category Service Added Category Controller Added Test Cases Moved Classes and Packages from Test to src, added by mistake still need to added @Service annotation to Category Service

---
+++ src/main/java/guru/springframework/services/CategoryService.java
@@ -0,0 +1,13 @@
+package guru.springframework.services;
+
+import guru.springframework.Domain.Category;
+import guru.springframework.api.v1.model.CategoryDTO;
+
+import java.util.List;
+
+public interface CategoryService {
+
+    List<CategoryDTO> getAllCategories();
+
+    CategoryDTO getCategoryByName(String name);
+}
---
+++ src/main/java/guru/springframework/services/CategoryServiceImpl.java
@@ -0,0 +1,33 @@
+package guru.springframework.services;
+
+import guru.springframework.api.v1.mapper.CategoryMapper;
+import guru.springframework.api.v1.model.CategoryDTO;
+import guru.springframework.repositories.CategoryRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CategoryServiceImpl implements CategoryService {
+
+    private final CategoryRepository categoryRepository;
+    private final CategoryMapper categoryMapper;
+
+    public CategoryServiceImpl(CategoryRepository categoryRepository, CategoryMapper categoryMapper) {
+        this.categoryRepository = categoryRepository;
+        this.categoryMapper = categoryMapper;
+    }
+
+    @Override
+    public List<CategoryDTO> getAllCategories() {
+        return categoryRepository
+                .findAll()
+                .stream()
+                .map(categoryMapper::categoryToCategoryDTO)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public CategoryDTO getCategoryByName(String name) {
+        return categoryMapper.categoryToCategoryDTO(categoryRepository.findByName(name));
+    }
+}
