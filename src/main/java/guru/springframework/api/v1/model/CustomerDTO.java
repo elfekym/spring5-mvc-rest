e916da1a412a9becc2c53e6a201d9a44792f04df
Mohamed <mohamed.e@trufla.com>
2019-07-04 16:51:36 +0200

    Added Swagger Dep, and Configuration class, with supporting info

--- src/main/java/guru/springframework/api/v1/model/CustomerDTO.java
+++ src/main/java/guru/springframework/api/v1/model/CustomerDTO.java
@@ -1,11 +1,17 @@
 package guru.springframework.api.v1.model;
 
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CustomerDTO {
 
     //private Long id;
+    @ApiModelProperty(value = "this is the first name", required = true)
     private String firstName;
     private String lastName;
     private String customerURL;
--- src/main/java/guru/springframework/controllers/v1/CustomerController.java
+++ src/main/java/guru/springframework/controllers/v1/CustomerController.java
@@ -3,9 +3,12 @@ package guru.springframework.controllers.v1;
 import guru.springframework.api.v1.model.CustomerDTO;
 import guru.springframework.api.v1.model.CustomerListDTO;
 import guru.springframework.services.CustomerService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+@Api(value = "Customer")
 @RestController
 @RequestMapping(CustomerController.BASE_URL)
 public class CustomerController {
@@ -17,6 +20,7 @@ public class CustomerController {
         this.customerService = customerService;
     }
 
+    @ApiOperation(value = "This will get a list of Customers.")
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public CustomerListDTO getListofCustomers(){
@@ -25,30 +29,35 @@ public class CustomerController {
 
     }
 
+    @ApiOperation(value = "This will get a customer by ID.")
     @GetMapping({"{id}"})
     @ResponseStatus(HttpStatus.OK)
     public CustomerDTO getCustomerById(@PathVariable Long id){
         return customerService.getCustomerById(id);
     }
 
+    @ApiOperation(value = "This will create a customer.")
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public CustomerDTO createNewCustomer(@RequestBody CustomerDTO customerDTO){
         return customerService.createNewCustomer(customerDTO);
     }
 
+    @ApiOperation(value = "This will edit a customer.")
     @PutMapping("{id}")
     @ResponseStatus(HttpStatus.OK)
     public CustomerDTO updateCustomer(@PathVariable Long id, @RequestBody CustomerDTO customerDTO){
         return customerService.saveCustomer(id, customerDTO);
     }
 
+    @ApiOperation(value = "This will update a specific field in a customer.")
     @PatchMapping({"/{id}"})
     @ResponseStatus(HttpStatus.OK)
     public CustomerDTO patchCustomer(@PathVariable Long id, @RequestBody CustomerDTO customerDTO){
         return customerService.patchCustomer(id, customerDTO);
     }
 
+    @ApiOperation(value = "This will delete a customer by it's id.")
     @DeleteMapping({"/{id}"})
     @ResponseStatus(HttpStatus.OK)
     public void deleteCustomer(@PathVariable Long id){
