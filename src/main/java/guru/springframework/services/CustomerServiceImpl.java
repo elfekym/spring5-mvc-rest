fb0505d7ee196c810be78ba9a264e5e3ba84f295
Mohamed <mohamed.e@trufla.com>
2019-07-04 10:07:45 +0200

    added patch service, Implemented Integration Test too,

--- src/main/java/guru/springframework/services/CustomerServiceImpl.java
+++ src/main/java/guru/springframework/services/CustomerServiceImpl.java
@@ -59,6 +59,23 @@ public class CustomerServiceImpl implements CustomerService {
 
         return saveAndReturnDTO(customer);
     }
+
+    @Override
+    public CustomerDTO patchCustomer(Long id, CustomerDTO customerDTO) {
+        return customerRepository.findById(id).map(customer -> {
+
+            if(customerDTO.getFirstName() != null){
+                customer.setFirstName(customerDTO.getFirstName());
+            }
+
+            if(customerDTO.getLastName() != null){
+                customer.setLastName(customerDTO.getLastName());
+            }
+
+            return customerMapper.customerToCustomerDTO(customerRepository.save(customer));
+        }).orElseThrow(RuntimeException::new); //todo implement better exception handling;
+    }
+
     private CustomerDTO saveAndReturnDTO(Customer customer) {
         Customer savedCustomer = customerRepository.save(customer);
 
---
+++ src/main/java/guru/springframework/services/CustomerServiceImplIT.java
@@ -0,0 +1,103 @@
+package guru.springframework.services;
+
+import guru.springframework.Domain.Customer;
+import guru.springframework.api.v1.mapper.CustomerMapper;
+import guru.springframework.api.v1.model.CustomerDTO;
+import guru.springframework.bootstrap.Bootstrap;
+import guru.springframework.repositories.CategoryRepository;
+import guru.springframework.repositories.CustomerRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.hamcrest.core.IsNot.not;
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class CustomerServiceImplIT {
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @Autowired
+    CategoryRepository categoryRepository;
+
+    CustomerService customerService;
+
+    @Before
+    public void setUp() throws Exception {
+        System.out.println("Loading Customer Data");
+        System.out.println(customerRepository.findAll().size());
+
+        //setup data for testing
+        Bootstrap bootstrap = new Bootstrap(categoryRepository, customerRepository);
+        bootstrap.run(); //load data
+
+        customerService = new CustomerServiceImpl(customerRepository, CustomerMapper.INSTANCE);
+    }
+
+    @Test
+    public void patchCustomerUpdateFirstName() throws Exception {
+        String updatedName = "UpdatedName";
+        long id = getCustomerIdValue();
+
+        Customer originalCustomer = customerRepository.getOne(id);
+        assertNotNull(originalCustomer);
+        //save original first name
+        String originalFirstName = originalCustomer.getFirstName();
+        String originalLastName = originalCustomer.getLastName();
+
+        CustomerDTO customerDTO = new CustomerDTO();
+        customerDTO.setFirstName(updatedName);
+
+        customerService.patchCustomer(id, customerDTO);
+
+        Customer updatedCustomer = customerRepository.findById(id).get();
+
+        assertNotNull(updatedCustomer);
+        assertEquals(updatedName, updatedCustomer.getFirstName());
+        assertThat(originalFirstName, not(equalTo(updatedCustomer.getFirstName())));
+        assertThat(originalLastName, equalTo(updatedCustomer.getLastName()));
+    }
+
+    @Test
+    public void patchCustomerUpdateLastName() throws Exception {
+        String updatedName = "UpdatedName";
+        long id = getCustomerIdValue();
+
+        Customer originalCustomer = customerRepository.getOne(id);
+        assertNotNull(originalCustomer);
+
+        //save original first/last name
+        String originalFirstName = originalCustomer.getFirstName();
+        String originalLastName = originalCustomer.getLastName();
+
+        CustomerDTO customerDTO = new CustomerDTO();
+        customerDTO.setLastName(updatedName);
+
+        customerService.patchCustomer(id, customerDTO);
+
+        Customer updatedCustomer = customerRepository.findById(id).get();
+
+        assertNotNull(updatedCustomer);
+        assertEquals(updatedName, updatedCustomer.getLastName());
+        assertThat(originalFirstName, equalTo(updatedCustomer.getFirstName()));
+        assertThat(originalLastName, not(equalTo(updatedCustomer.getLastName())));
+    }
+
+    private Long getCustomerIdValue(){
+        List<Customer> customers = customerRepository.findAll();
+
+        System.out.println("Customers Found: " + customers.size());
+
+        //return first id
+        return customers.get(0).getId();
+    }
+}
